cf2537b98f91b98363a8d9583da2c2cfd91e61b1
Jacob Tardieu <jacob@tardieu.net>
2013-11-24 23:12:14 +0100

    Commit pré-dodo

--- src/one/OpenNebulaNode.java
+++ src/one/OpenNebulaNode.java
@@ -1,8 +1,15 @@
 package one;
 
+
+import java.util.Iterator;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.opennebula.client.Client;
 import org.opennebula.client.ClientConfigurationException;
 import org.opennebula.client.vm.VirtualMachine;
+import org.opennebula.client.vm.VirtualMachinePool;
 
 import terminal.User;
 
@@ -38,7 +45,6 @@ public class OpenNebulaNode {
 			
 			int id = Integer.parseInt(stringId);
 			VirtualMachine vm = new VirtualMachine(id,oneClient);
-			vm.stop();
 			vm.delete();
 			System.out.println("VM "+stringId+" terminated");
 		} else {
@@ -60,6 +66,20 @@ public class OpenNebulaNode {
 		}
 	}
 	
+	public void resume(String stringId) {
+		if (oneClient == null){
+			System.out.println("You need to connect to the node before trying to resume any VM");
+		} else if (stringId.length()>0){
+			
+			int id = Integer.parseInt(stringId);
+			VirtualMachine vm = new VirtualMachine(id,oneClient);
+			vm.resume();
+			System.out.println("VM "+stringId+" resumed");
+		} else {
+			System.out.println("You need to enter the id you want to resume");
+		}
+	}
+	
 	public void migrate(String stringId, String hostStringId) {
 		if (oneClient == null){
 			System.out.println("You need to connect to the node before trying to migrate any VM");
@@ -73,6 +93,42 @@ public class OpenNebulaNode {
 		} else {
 			System.out.println("You need to enter the id you want to migrate and the id of the host to receive the VM");
 		}
+	}
+	
+	public void listVMs() {
+		VirtualMachinePool vmPool = new VirtualMachinePool(oneClient);
+		Iterator<VirtualMachine> vms = vmPool.iterator();
+		VirtualMachine vm;
+		while (vms.hasNext()) {
+			vm = vms.next();
+			String info = vm.info().getMessage();
+			System.out.print("NAME : "+vm.getName()+"; ");
+			System.out.print("STATUS : "+vm.stateStr()+"; ");
+			
+			if (vm.state() == 3) {//VM running
+				String monitoring = vm.monitoring().getMessage();
+				try {
+					Document doc = DocumentHelper.parseText(monitoring);
+					System.out.print("Hostname: "+doc.selectSingleNode("//HOSTNAME").getText());
+                    System.out.print("Host ID: "+doc.selectSingleNode("//HID").getText());
+                    System.out.print("CPU: "+doc.selectSingleNode("//CPU").getText()+"%");
+                    System.out.println("Memory: "+doc.selectSingleNode("//MEMORY").getText()+" kB");
+				} catch (DocumentException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void countVMs() {
+		VirtualMachinePool vmPool = new VirtualMachinePool(oneClient);
+		Iterator<VirtualMachine> vms = vmPool.iterator();
+		int i = 0;
+		while (vms.hasNext()) {
+			vms.next();
+			i++;
+		}
+		System.out.println("There is "+i+" VMs on this host.");
 	}
 	
 }
--- src/terminal/Main.java
+++ src/terminal/Main.java
@@ -6,89 +6,104 @@ import one.OpenNebulaNode;
 
 import org.opennebula.client.Client;
 
-
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		Boolean quit = false;
 		String prompt = "";
 		String command = "";
-		String arg1 ="";
+		String arg1, arg2 = "";
+		String[] commands = null;
 		Scanner sc = new Scanner(System.in);
-		
+
 		OpenNebulaNode node = new OpenNebulaNode();
-		
+
 		System.out.println(" === Welcome in the CLI tool for ONE ===");
-		
-		while (!quit){
+
+		while (!quit) {
 			System.out.print("> ");
 			prompt = "";
 			command = "";
 			arg1 = "";
-			try{
+			arg2 = "";
+			try {
 				prompt = sc.nextLine();
 				command = prompt;
-				if (prompt.indexOf(' ') != -1){
-					command = prompt.substring(0,prompt.indexOf(' '));
-					arg1 = prompt.substring(prompt.indexOf(' '));
-				}
-				System.out.println(arg1);
-				switch (command){
-					case "connect":
-						node.connect(arg1);
-						break;
-					case "version":
-						node.printVersion();
-						break;
-					case "verify":
-						// TODO En dernier, quand on aura testé le reste (il faut verifier les retours)
-						break;
-					case "print":
-						switch(arg1) {
-							case "-VMs":
-								//TODO
-								break;
-							case "-nodes":
-								//TODO
-								break;
-							default:
-								System.out.println("Wrong arguments for print. See help");
+				if (prompt.indexOf(' ') != -1) {
+					commands = prompt.split(" ");
+					command = commands[0];
+					if (commands.length > 1) {
+						arg1 = commands[1];
+						if (commands.length > 2) {
+							arg2 = commands[2];
 						}
+					}
+				}
+				switch (command) {
+				case "connect":
+					node.connect(arg1);
+					break;
+				case "version":
+					node.printVersion();
+					break;
+				case "verify":
+					// TODO En dernier, quand on aura testé le reste (il faut
+					// verifier les retours)
+					break;
+				case "print":
+					switch (arg1) {
+					case "-VMs":
+						// TODO
 						break;
-					case "suspend":
-						node.suspend(arg1);
-						break;
-					case "migrate":
-						node.migrate(null, null); //TODO					
-						break;
-					case "terminate":
-						node.terminate(arg1);
-						break;
-					case "exit":
-						quit = true;
-						break;
-					case "help":
-						showHelp();
+					case "-nodes":
+						// TODO
 						break;
 					default:
-						System.out.println("That command is not handled by the system");
-						break;
+						System.out.println("Wrong arguments for print. See help");
+					}
+					break;
+				case "suspend":
+					node.suspend(arg1);
+					break;
+				case "resume":
+					node.resume(arg1);
+					break;
+				case "migrate":
+					node.migrate(arg1, arg2); // TODO
+					break;
+				case "terminate":
+					node.terminate(arg1);
+					break;
+				case "exit":
+					quit = true;
+					break;
+				case "help":
+					showHelp();
+					break;
+				default:
+					System.out.println("That command is not handled by the system");
+					break;
 				}
-			} catch (Exception e){
-				
+			} catch (Exception e) {
+
 			}
 		}
 	}
-	
-	public static void showHelp(){
+
+	public static void showHelp() {
 		System.out.println("Help :");
 		System.out.println("Commands available :");
 		System.out.println("connect <adressOfNode>");
 		System.out.println("version");
-		//TODO complete for the commands to do
+		System.out.println("print -VMs");
+		System.out.println("print -nodes");
+		System.out.println("suspend <VM ID>");
+		System.out.println("resume <VM ID>");
+		System.out.println("terminate <VM ID>");
+		System.out.println("migrate <VM ID> <host ID>");
 		System.out.println("exit");
-		System.out.println("help");		
+		System.out.println("help");
 	}
 
 }
